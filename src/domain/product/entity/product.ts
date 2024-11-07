a76e08ea5b2d6d47f48fedf45db3ab36eaa72291
venilson2 <venilson2009@hotmail.com>
2024-11-07 19:48:46 -0300

    Add validation

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,17 +1,29 @@
-export default class Product {
-  private _id: string = "";
+import Entity from "../../@sahred/entity/entity.abstract";
+import NotificationError from "../../notification/notification.error";
+
+export default class Product extends Entity {
   private _name: string = "";
 
   constructor(id: string, name: string) {
+    super();
     this._id = id;
     this._name = name;
-  }
-
-  get id(): string {
-    return this._id;
+    this.validate();
+    if(this.notification.hasErrors()){
+      throw new NotificationError(this.notification.getErrors())
+    }
   }
 
   get name(): string {
     return this._name;
+  }
+
+  validate(){
+    if(this._id.length === 0){
+      this.notification.addError({context: "product", message: "Id is required"})
+    }
+    if(this._name.length === 0){
+      this.notification.addError({context: "product", message: "Name is required"})
+    }
   }
 }
